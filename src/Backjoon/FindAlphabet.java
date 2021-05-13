0e0f98dc9147a8799fd9afef5a82c89ac05c1a09
hkee <30564518+hkee@users.noreply.github.com>
2021-05-13 22:38:19 +0900

    Add files via upload
    문자열-알파벳찾기

---
+++ src/Backjoon/FindAlphabet.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class FindAlphabet {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		for(int i=0;i<26;i++) {
+			System.out.print(s.indexOf('a'+i)+" ");
+		}
+		
+	}
+}
