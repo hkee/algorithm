fae3db3f0142bf46bb83113bf1850a5f559b909d
hyunkyung park <hk041280@gmail.com>
2021-05-23 23:37:48 +0900

    dfs-섬의개수 수정

--- src/Backjoon/IslandCount.java
+++ src/Backjoon/IslandCount.java
@@ -8,11 +8,12 @@ public class IslandCount {
 	static int w,h;
 	static int dx[] = {-1,0,1,-1,1,-1,0,1};
 	static int dy[] = {-1,-1,-1,0,0,1,1,1};
-	static int totalCount=0;
+	static int totalCount;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String temp = br.readLine();
 		do {
+			totalCount=0;
 			String input[] = temp.split(" ");
 			w = Integer.parseInt(input[0]);
 			h = Integer.parseInt(input[1]);
@@ -32,6 +33,7 @@ public class IslandCount {
 					}
 				}
 			}
+			System.out.println(totalCount);
 
 			temp = br.readLine();
 		}while(!temp.equals("0 0"));
@@ -39,7 +41,14 @@ public class IslandCount {
 	public static void findIsland(int y,int x,int map[][]) {
 		map[y][x] = 0;
 		for(int c=0;c<dx.length;c++) {
-			
+			int nextY = y+dy[c];
+			int nextX = x+dx[c];
+			if(nextY<0||nextX<0||nextY>=h||nextX>=w) {
+				continue;
+			}
+			if(map[nextY][nextX]==1) {
+				findIsland(nextY, nextX, map);
+			}
 		}
 	}
 
