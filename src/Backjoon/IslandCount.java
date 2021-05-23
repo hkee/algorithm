a93f2bcbcb761108105a793c68b9b1e1db124d32
hyunkyung park <hk041280@gmail.com>
2021-05-23 23:24:07 +0900

    dfs알고리즘-섬의개수

--- src/Backjoon/IslandCount.java
+++ src/Backjoon/IslandCount.java
@@ -1,16 +1,46 @@
-package baekjoon;
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class IslandCount {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static int w,h;
+	static int dx[] = {-1,0,1,-1,1,-1,0,1};
+	static int dy[] = {-1,-1,-1,0,0,1,1,1};
+	static int totalCount=0;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String temp = br.readLine();
+		do {
+			String input[] = temp.split(" ");
+			w = Integer.parseInt(input[0]);
+			h = Integer.parseInt(input[1]);
+			int map[][] = new int[h][w];
+			//map 입력받기
+			for(int i=0;i<h;i++) {
+				String inputMap[] = br.readLine().split(" ");
+				for(int j=0;j<w; j++) {
+					map[i][j]=Integer.parseInt(inputMap[j]);
+				}
+			}
+			for(int i=0;i<h;i++) {
+				for(int j=0;j<w;j++) {
+					if(map[i][j]==1) {
+						totalCount++;
+						findIsland(i,j,map);
+					}
+				}
+			}
 
-
-
-
-
-
-
+			temp = br.readLine();
+		}while(!temp.equals("0 0"));
+	}
+	public static void findIsland(int y,int x,int map[][]) {
+		map[y][x] = 0;
+		for(int c=0;c<dx.length;c++) {
+			
+		}
 	}
 
 }
