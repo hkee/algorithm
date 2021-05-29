81d34901ccb41ec8b3f584ad4d0e03003d1c026d
hyunkyung park <hk041280@gmail.com>
2021-05-29 15:51:35 +0900

    시뮬레이션+bfs알고리즘-아기상어

--- README.md
+++ README.md
@@ -37,6 +37,7 @@ Number   | Name       | File Name
 4963     |섬의 개수     | IslandCount.java
 1012     | 유기농 배추  | Cabbage.java
 2644     | 촌수 계산    | FamilyCount.java
+16236    | 아기상어     | BabyShark.java
 ---------
 Samsung swexpert
 ============
--- src/Backjoon/BabyShark.java
+++ src/Backjoon/BabyShark.java
@@ -2,7 +2,9 @@ package Backjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class fish {
     int x;
@@ -18,31 +20,97 @@ class fish {
 public class BabyShark {
     static int N;
     static int map[][];
+    static int dx[]={0,1,0,-1};
+    static int dy[]={1,0,-1,0};
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
-        int sx,sy;
+        int visited[][] = new int[N][N];
+        int sx=0,sy=0;
         for(int i=0;i<N;i++) {
             String temp[] = br.readLine().split(" ");
             for(int j=0;j<N;j++) {
                 if(temp[j].equals("9")) {
                     sx=j;
                     sy=i;
+                    map[i][j] = 0;
+                }else {
+                    map[i][j] = Integer.parseInt(temp[j]);
+
                 }
-                map[i][j] = Integer.parseInt(temp[j]);
             }
         }
 
-        ArrayList<fish> edibleFish = new ArrayList<>();
 
-        int sSize = 1;
+        int sSize = 2;
         int level = 0;
+        boolean go = true;
+        int result = 0;
 
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<N;j++) {
-                edibleFish.add(new fish(j,i,i*4+j));
+        while(go) {
+            for(int c=0;c<N;c++) {
+                Arrays.fill(visited[c],-1);
             }
+            //탐색
+            Queue<fish> q = new LinkedList<>();
+            q.offer(new fish(sx, sy, 0));
+            visited[sy][sx]=0;
+            int min = Integer.MAX_VALUE;
+            int ml = N*N;
+            
+            while(!q.isEmpty()) {    
+                fish l = q.poll();
+                int nextX=0,nextY=0;
+                for(int i=0;i<4;i++) {
+                    nextX = l.x+dx[i];
+                    nextY = l.y+dy[i];
+                    if(nextX<0 || nextY<0 || nextX>=N || nextY>=N) {
+                        continue;
+                    }
+                    if(visited[nextY][nextX]==-1 && map[nextY][nextX]<=sSize) {
+                        fish tempF = new fish(nextX,nextY,nextY*N+nextX);
+                        visited[nextY][nextX]=visited[l.y][l.x]+1;
+                        if(min<visited[nextY][nextX]) {
+                            continue;
+                        }
+                        q.offer(tempF);
+                        if(0<map[nextY][nextX] && map[nextY][nextX]<sSize) {
+                            //먹을 수 있는 물고기
+                            if(min>visited[nextY][nextX]) {
+                                min = visited[nextY][nextX];
+                                ml = tempF.no;
+                            } else if(min==visited[nextY][nextX]) {
+                                if(ml>tempF.no) {
+                                    ml = tempF.no;
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+            //먹기
+            if(ml!=N*N) {
+                map[ml/N][ml%N] = 0;
+                sy=ml/N;
+                sx=ml%N;
+                result+=min;
+                level++;
+                //level체크
+                if(sSize==level) {
+                    //탐색부터 다시시작
+                    level=0;
+                    sSize++;
+                }    
+            }else {
+                break;
+            }
+            
+            
+            
         }
+        System.out.println(result);
+        
     }
 }
