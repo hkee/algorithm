f522580eaee3585880cc2c64dd6d49c3909b9a78
hyunkyung park <hk041280@gmail.com>
2021-06-06 23:17:37 +0900

    문자열-단어정렬

---
+++ src/Backjoon/WordSort.java
@@ -0,0 +1,12 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class WordSort {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+    }
+}
