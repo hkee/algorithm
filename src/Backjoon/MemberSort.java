6ad279160ddea5fe7d2315c732c6466e16b7cb35
hyunkyung park <hk041280@gmail.com>
2021-06-09 23:34:32 +0900

    test

--- src/Backjoon/MemberSort.java
+++ src/Backjoon/MemberSort.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 public class MemberSort {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        br.readLine();
 
     }
 }
