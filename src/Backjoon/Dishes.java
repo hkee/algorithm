9e2b9f6b27e9a6659ffeae8575af374fdef8cea6
hyunkyung park <hk041280@gmail.com>
2021-07-02 23:40:46 +0900

    문자열-접시

---
+++ src/Backjoon/Dishes.java
@@ -0,0 +1,12 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Dishes {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+    }
+}
